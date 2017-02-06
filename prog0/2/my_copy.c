ad57b2c4ce7928ae4da62271bc1cce1bb6bb2230
mattprice09 <mattprice09@rocketmail.com>
2017-02-06 15:18:14 -0600

    Part 2 finished

--- prog0/2/my_copy.c
+++ prog0/2/my_copy.c
@@ -1,6 +1,8 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
   if (argc < 3) {
@@ -12,27 +14,34 @@ int main(int argc, char **argv) {
   char *outfile = argv[2];
 
   /* Open file for reading */
-  FILE *f = fopen(infile, "r");
-  if (!f) {
+  int f = open(infile, O_RDONLY);
+  if (f < 0) {
     printf("> ERROR opening file %s. Please make sure the input file exists.", infile);
     return 1;
   }
 
   /* Open file for writing */
-  FILE *outf = fopen(outfile, "w");
-  if (!outf) {    
+  int outf = open(outfile, O_RDWR | O_TRUNC | O_CREAT, 0666);
+  if (outf < 0) {    
     printf("> ERROR opening file %s for writing. Please try again.", outfile);
     return 1;
   }
 
-  /* Read file contents*/
+  /* Hard-code a break when it's the end of file because no simply way to reassign initialized char array */ 
   char buffer[100];
-  fread(buffer, 1, 200, f);
+  int readSize = read(f, buffer, sizeof(buffer));
+  while (readSize > 0) {
+
+    /* write chunk */
+    write(outf, buffer, readSize);
+
+    /* read next chunk */
+    readSize = read(f, buffer, sizeof(buffer));    
+  }   
+ 
+  /* Close files and exit */ 
+  close(f);
+  close(outf);
   
-  /* Write to user-specified file */
-  fwrite(buffer, sizeof(char), strlen(buffer), outf);
-  
-  fclose(f);
-  fclose(outf);
   return 0;
 }
